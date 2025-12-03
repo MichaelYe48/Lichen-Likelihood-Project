1938e04b92583335378b789b14b972a31e66bf8b
sienaokuno <sienaokuno@gmail.com>
2025-12-02 19:25:08 -0800

    data parsing for element nodes working. need to implement binning for non-element nodes

--- data_parsing.py
+++ data_parsing.py
@@ -4,6 +4,8 @@ import csv
 import matplotlib.pyplot as plt
 
 
+
+
 def loadData(file_name):
     '''
     Loads csv containing element analysis data
@@ -13,57 +15,120 @@ def loadData(file_name):
     Output:
         element_df - pandas DataFrame containing loaded data
     '''
-    #force element cols to be floats
+    #read
     with open(file_name, 'r', newline='') as file:
         csv_reader = csv.reader(file)
-        element_headers = [h for h in next(csv_reader) if 'dw' in h]
-    typeDict = dict.fromkeys(element_headers, np.float64)
-    
-    element_df = pd.read_csv(file_name, engine='python', on_bad_lines='skip',
-                             dtype=typeDict, na_values=['n.d.','nd','n.d'],keep_default_na=True)
-    return element_df
+        target_headers = [h for h in next(csv_reader) if 'dw' in h or 'year' in h.lower() or 'pollution' in h.lower()]
+
+    df = pd.read_csv(file_name, engine='python', on_bad_lines='skip',
+                     na_values=['n.d.','nd','n.d'],keep_default_na=True)
+
+    return df
 
 
 
-def filterByElement(name, element_df, min=None, max=None, verbose=False):
+def filterByNode(name, df, min=None, max=None, isNum = True, verbose=False):
     '''
-    Filters DataFrame based on a single column
+    Filters DataFrame based on a single column to remove NaN
 
     Input:
         name - string to search for within column headers. Does not need to be entire name
-        element_df - pandas DataFrame containing loaded data
+        df - pandas DataFrame containing loaded data
     Output:
         filtered_df - pandas dataframe containing filtered data
     '''
     #Get desired column, filter for NaNs
-    header = [col_name for col_name in element_df.columns if name.lower() in col_name.lower()]
-    assert len(header)==1, f"Expected 1 column to be fetched for name {name}. Got: {len(header)}"
+    header = [col_name for col_name in df.columns if name.lower() in col_name.lower()]
+    assert len(header)>0, f"Expected 1 column to be fetched for name {name}. Got: {len(header)}"
     col = header[0]
 
-    #Filter out NaN and min/max, if passed in
-    filtered_df = element_df.dropna(subset=[col])
-    if min!=None:
-        filtered_df = filtered_df[filtered_df[col] >= min]
-    if max!=None:
-        filtered_df = filtered_df[filtered_df[col] <= max]
+    #Filter
+    filtered_df = df.copy()
+    if isNum:
+        filtered_df[col] = pd.to_numeric(df[col], errors='coerce')
+        #Min / max
+        if min!=None:
+            filtered_df = filtered_df[filtered_df[col] >= min]
+        if max!=None:
+            filtered_df = filtered_df[filtered_df[col] <= max]
+
+    filtered_df.dropna(subset=col, inplace=True)
 
     #Look at data
     if verbose:
         print(filtered_df[col].describe())
-        hist = df[col].hist(bins=1000)
+        #hist = df[col].hist(bins=1000)
         plt.show()
 
-    return filtered_df
+    return filtered_df, col
 
 
 
+def binElementNode(colName, df):
+    '''
+    Creates new column in df to store binned values for given element.
+    -based on value percentiles
+    '''
+    newColName = colName + '_binned'
+    bins = ['low', 'medium', 'high']
+    df[newColName] = pd.qcut(df[colName], q=3, labels=bins, retbins=True)
 
-if __name__ == "__main__":
-    path = 'air_lichen_query.csv'
-    df = loadData(path)
-    #Copper, Chromium, Nickel, Iron
-    df_filtered = df.copy()
-    print(f'Original df size: {len(df_filtered)}')
-    cols = ['copper','chromium','nickel','iron','year']
-    for i in cols:
-        filterByElement(i,df_filtered,verbose=True)
+    return df, newColName
+
+
+def binSpecialNode(colName, df, bins, values):
+    '''
+    Creates new column in df to store binned values for given node.
+    -based on values passed in
+    '''
+    newColName = colName + '_binned'
+    df[newColName] = pd.qcut(df[colName], q=3, labels=bins, retbins=True)
+
+
+#Load
+path = 'air_lichen_query.csv'
+df = loadData(path)
+
+#Elements
+element_list = [
+    'nitrogen',
+    'sulfur',
+    'phosphorous',
+    'lead',
+    'copper',
+    'chromium'
+    #'potassium', 'manganese'
+]
+
+#Other nodes
+num_list = [
+    'Year of tissue collection',
+    'Air pollution score'
+]
+notNum_list = [
+    'region',
+    'Code for scientific name'
+]
+
+#Filter
+df_mod = df.copy()
+colNames = []
+for n in element_list+num_list:
+    df_mod, newCol = filterByNode(n,df_mod,verbose=True)
+    colNames.append(newCol)
+
+for n in notNum_list:
+    df_mod, newCol = filterByNode(n,df_mod,isNum=False,verbose=True)
+    colNames.append(newCol)
+
+#Bin
+bin_names = []
+for n in element_list+num_list:
+    df_mod, binCol = binElementNode(n,df_mod,verbose=True)
+    bin_names.append(newCol)
+
+
+print(df_mod.shape)
+
+#Final DF:
+final_df = df_mod[bin_names]
